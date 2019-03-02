a81a8d19b4c76e94d26a1eec65c94147a827936b
Samuel Lawrentz <samuellawrentz@gmail.com>
2019-03-03 00:17:13 +0530

    Added solutions and about pages

--- main.js
+++ main.js
@@ -19,6 +19,43 @@ function WebmailViewModel() {
             content: ['Artificial Intelligence', 'Machine Learning', 'Design Thinking', 'Software Consulting']
         }
     ];
+
+    self.solutions = [
+        'Artificial Intelligence',
+        'Augmented Reality Solutions',
+        'Internet of Things',
+        'Big Data Analytics',
+        'Cloud Computing',
+        'Blockchain Development'
+    ];
+
+    self.scrollTo = function(selector){
+        $('html, body').animate({
+            scrollTop: $($(selector)[0]).offset().top
+        }, 1000);
+    }
 };
 
 ko.applyBindings(new WebmailViewModel());
+
+var sections = {},
+_height  = $(window).height(),
+i        = 0;
+
+// Grab positions of our sections 
+$('.slide').each(function(){
+sections[$(this).attr('name')] = $(this).offset().top - 30;
+});
+
+$(document).scroll(function(){
+var pos = $(this).scrollTop();
+
+// Look in the sections object and see if any section is viewable on the screen. 
+// If two are viewable, the lower one will be the active one. 
+for(i in sections){
+    if(pos > sections[i] && pos < sections[i] + _height){
+        $('header li').removeClass('active');
+        $('#'+i).addClass('active');
+    }  
+}
+});
